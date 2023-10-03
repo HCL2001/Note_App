cb782e92af8aa353c06a20df37e3698e4df02647
Long Hoang <longhoang12212@gmail.com>
2023-10-03 09:33:55 +0700

    Note App

---
+++ script.js
@@ -0,0 +1,50 @@
+const noteContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+  noteContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", noteContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/delete.png";
+  noteContainer.appendChild(inputBox).appendChild(img);
+  updateStorage();
+  return;
+});
+
+noteContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+    return;
+  }
+  if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage;
+      };
+    });
+    return;
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    e.preventDefault();
+    return;
+  }
+});
